80aa3f2d7a0c529ff2215cc39bca213c47b9bb13
tatsianaDr <gitpasswordt1>
2020-05-15 13:05:12 +0300

    Changed app with notes

--- app_predict/service/app.py
+++ app_predict/service/app.py
@@ -1,51 +1,50 @@
-from flask import Flask, request
+import traceback
 
+from flask import Flask, jsonify, request
 import pickle
 import sklearn
-
 import os, sys, inspect
-
-from waitress import serve
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
+from service import model
+from service import svm_model
+
+
 app = Flask(__name__)
 
 
+@app.before_first_request
+def load_model_to_app():
+    # Load the model
+    app.classifier = load_model(svm_model.SVMModel, 'service/model/model_sentiment_prediction.pkl')
+
+
 @app.route('/predict', methods=['POST'])
-def predict():
+def review_classifier():
     if request.method == 'POST':
         review = request.json['review']
-        return {'Model' : classifier['metadata'],
-                'Prediction': predict_sentiment(review)}
+        return predict_sentiment(app.classifier, review)
 
 
-class Model:
-    def __init__(self):
-        with open('service/model/model_sentiment_prediction.pkl', 'rb') as file:
-            self.classifier = pickle.load(file)
+def load_model(model, model_path):
+    # Load the model
+    return model.load_model(model, model_path)
 
-my_model = Model()
 
-classifier = my_model.classifier
-
-def predict_sentiment(review):
+def predict_sentiment(model, review):
+    # Make the prediction
     try:
-        print(review)
-        print(classifier['model'])
-        prediction = list(classifier['model'].predict([review]))
+        prediction = list(model['model'].predict([review]))
         if prediction[0] == 0:
             pred_text = 'Negative'
         else:
             pred_text = 'Positive'
-        return pred_text
+        return jsonify({'Prediction': pred_text})
     except:
-        return 'Error'
-
+        return traceback.format_exc()
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8000)
-    # app.run(host='0.0.0.0')
-
+    app.run(host='0.0.0.0')
